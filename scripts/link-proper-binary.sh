1960396d01b9ef7bb1742099a7b8708c169bec4a
Felix Schlitter <felixSchlitter@gmail.com>
2016-12-06 09:52:17 +1300

    Use obnoxious .cmd extension for windows

--- scripts/link-proper-binary.sh
+++ scripts/link-proper-binary.sh
@@ -6,8 +6,9 @@ if [[ $(uname) =~ ^Darwin ]]; then
     ln -s jq-osx-amd64 jq
 else
     if [[ $(uname) == MINGW* ]]; then
-        rm -f jq
+        rm -f jq jq.cmd
         ln -s jq-win32.exe jq
+        ln -s jq-win32.exe jq.cmd
     else
         if [[ $(uname -m) =~ i686 ]]; then
             ln -s jq-linux32 jq
